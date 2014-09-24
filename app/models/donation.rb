6eb7b6c05519e867469c32807ba68abc4f1fed0d
hqcasanova <hqcasanova@users.noreply.github.com>
2014-09-24 15:12:48 -0400

    Verified first scaffold

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,6 @@ class Donation < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   belongs_to :backer, class_name: "User"
+
+  # validates :amount, presence: true, numericality: true
 end
