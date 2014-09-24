aebb182a83d5d65bafc6cf87284062d7cad2630b
Rani Itani <itani.rani@gmail.com>
2014-09-24 16:43:07 -0400

    fixed routing issue foruser signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/:id' => 'users#show', as: 'user'
+  
 
   get 'pages/index'
 
   devise_for :users
   resources :projects
 
+  get 'users/:id' => 'users#show', as: 'user'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
